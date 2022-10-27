a59c2b1c270c13d68e787278f83c95368050953c
Bastien64 <95182085+Bastien64@users.noreply.github.com>
2022-10-27 18:58:43 +0200

    mise a jour

--- ecf.sql
+++ ecf.sql
@@ -1,31 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1deb5ubuntu1
--- https://www.phpmyadmin.net/
---
--- Hôte : localhost:3306
--- Généré le : sam. 22 oct. 2022 à 23:48
--- Version du serveur : 8.0.30-0ubuntu0.22.04.1
--- Version de PHP : 8.1.2
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de données : `ecf`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `boutique`
---
 
 CREATE TABLE `boutique` (
   `id` int NOT NULL,
--- elementphp/ifnavbar.php
+++ elementphp/ifnavbar.php
@@ -10,5 +10,9 @@
             <a href="ajouterunefranchise.php">Ajouter une Franchise</a>
             </br><img class="logo" src="image/return.svg" alt="">
             <a href="ajouteruneboutique.php">Ajouter une ou des boutiques</a>
-
         </div> 
+
+        <div>
+            <img class="logo" src="image/3-circle.svg" alt="">
+            <a href="touteslesfranchises.php">Modifier des permissions</a>
+        </div>
